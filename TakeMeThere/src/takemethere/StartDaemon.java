5ffa25ffbad52c85c3baad5b15c9ecdf7543a3fd
Kashikoku <Kashikoku@DESKTOP-UPC7N9I>
2019-03-04 17:18:49 -0300

    First commmit

---
+++ TakeMeThere/src/takemethere/StartDaemon.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package takemethere;
+
+import javax.swing.JFrame;
+import takemethere.MainScreen;
+/**
+ *
+ * @author Kashikoku
+ */
+public class StartDaemon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainScreen frame = new MainScreen();
+        frame.setVisible(true);
+        
+        //frame.formatCoordinates("https://www.openstreetmap.org/#map=18/21.27041/-157.81816");
+    }
+    
+}
